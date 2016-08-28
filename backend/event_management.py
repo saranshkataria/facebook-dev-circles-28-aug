2f8539f04c3d35f2f9f3fdb8bf1e06d8b55417e9
Akul Mehra <mehra.akul@gmail.com>
2016-08-28 16:03:55 +0530

    add routes setup

--- backend/event_management.py
+++ backend/event_management.py
@@ -7,5 +7,49 @@ db = SQLAlchemy(app)
 
 from app import views, models
 
+
+@app.route('/')
+def home():
+
+
+@app.route('/login')
+def login():
+
+
+@app.route('/logout')
+def logout():
+
+
+@app.route('/event/<id>')
+def event():
+
+
+@app.route('/event/<id>/query')
+def query():
+
+
+@app.route('/event/<id>/import_csv')
+def import_csv():
+
+
+@app.route('/event/<id>/attendees')
+def attendees():
+
+
+@app.route('/event/<id>/send_invite')
+def send_invite():
+
+
+@app.route('/event/<id>/edit')
+def edit():
+
+
+@app.route('/event/<id>/stats')
+def stats():
+
+
+@app.route('/validate_qr')
+def validate_qr():
+
 if __name__ == "__main__":
     app.run(debug=True)
