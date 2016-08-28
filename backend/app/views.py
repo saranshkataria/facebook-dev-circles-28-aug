b0f20e7701c7852fe679ee6c08a99e20a3c539af
arsh23 <programmer.arsh@gmail.com>
2016-08-28 16:37:55 +0530

    fixed import error

--- backend/app/views.py
+++ backend/app/views.py
@@ -5,55 +5,66 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-from app import views, models
-
 
 @app.route('/')
 def home():
     pass
 
+
 @app.route('/register')
 def register():
     pass
+
 
 @app.route('/login')
 def login():
     pass
 
+
 @app.route('/logout')
 def logout():
     pass
+
 
 @app.route('/event/<id>')
 def event():
     pass
 
+
 @app.route('/event/<id>/query')
 def query():
     pass
+
 
 @app.route('/event/<id>/import_csv')
 def import_csv():
     pass
 
+
 @app.route('/event/<id>/attendees')
 def attendees():
+    pass
+
 
 @app.route('/event/<id>/attendees/add_walkin')
 def add_walkin():
     pass
 
+
 @app.route('/event/<id>/send_invite')
 def send_invite():
     pass
+
 
 @app.route('/event/<id>/edit')
 def edit():
     pass
 
+
 @app.route('/event/<id>/stats')
 def stats():
     pass
+
 
 @app.route('/validate_qr')
 def validate_qr():
