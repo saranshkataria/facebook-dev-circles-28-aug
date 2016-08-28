fd5d6c1cc1ad01918b7350f7109046b6dc1bd2c4
Manraj Singh <manrajsinghgrover@gmail.com>
2016-08-28 16:54:44 +0530

    Frontend added

---
+++ Frontend-master/src/components/events.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Events extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>MyComponent</div>
+    );
+  }
+}
---
+++ Frontend-master/src/components/navbar.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+Navbar.propTypes = {
+};
